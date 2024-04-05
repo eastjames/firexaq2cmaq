da8941de217480d0be08c56ee1ec54321b877259
eastjames <47705516+eastjames@users.noreply.github.com>
2024-04-04 22:16:39 -0400

    Organic aerosol from aerosol mass spec

--- run.sh
+++ run.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+# get the data
+
+ddir=data/ams/
+mkdir -p $ddir
+curl -L https://www-air.larc.nasa.gov/img/tmp/WWW-AIR_1712276024307.zip -o ${ddir}/rawdata.zip
+unzip ${ddir}/rawdata.zip -d ${ddir}
+
+ddir=data/smoke_flag/
+mkdir -p $ddir
+curl -L https://www-air.larc.nasa.gov/img/tmp/WWW-AIR_1712278888480.zip -o ${ddir}/rawdata.zip
+unzip ${ddir}/rawdata.zip -d ${ddir}
+
 #conda activate jpy01
-python firexaq2cmaq.py 20190801 20190807 108NHEMI2 10
+python run/firexaq2cmaq.py 20190801 20190807 108NHEMI2 10
 
 for f in ./gridded/*.nc
 do
